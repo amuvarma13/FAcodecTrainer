cfcd87fa5b550c7aa5d27fdc6f36c92b4c355b3d
Amartya Varma <amartya.varma@gmail.com>
2024-10-20 01:19:36 -0700

    F

--- download_wavs.py
+++ download_wavs.py
@@ -3,6 +3,7 @@ from datasets import DatasetDict
 import soundfile as sf
 from datasets import load_dataset
 import numpy as np
+import librosa
 
 
 dsn = "amuvarma/audio-lunatrejo-0"
@@ -36,15 +37,12 @@ def save_audio_to_wav(dataset: DatasetDict, output_folder: str):
         audio = example['audio']
         
         # Check if 'audio' is a dictionary (which is likely if it's from datasets library)
-        if isinstance(audio, dict):
-            audio_array = audio['array']
-            sampling_rate = audio['sampling_rate']
-            print("audio sampling rate", sampling_rate)
-        else:
-            # If it's not a dictionary, assume it's already an array
-            # You might need to adjust this part based on your actual data structure
-            audio_array = audio
-            sampling_rate = 16000  # You might need to change this default value
+        
+        audio_array = audio['array']
+        sampling_rate = audio['sampling_rate']
+        audio_array = librosa.resample(audio_array, orig_sr=sampling_rate, target_sr=24000)
+        print("audio sampling rate", sampling_rate)
+
         
         # Check if the audio_array is not empty and contains actual samples
         if isinstance(audio_array, np.ndarray) and audio_array.size > 0 and not np.all(audio_array == 0):
